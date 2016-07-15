65b6372eab4f883b111696078f27f886d110e89c
samhenrichs <eurostepedits@gmail.com>
2016-07-15 13:44:16 -0500

    Finished design. The only thing left is too implement images and the Amazon API as well as a good help system.

--- app/assets/javascripts/offers.js
+++ app/assets/javascripts/offers.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
 
+	$('#category-dropdown').hide(000);
+
 	$('#push').submit(function() {
 		$('#offer').trigger('submit.rails');
 	});
 	//push_form
 	//offer_form
+
+	$('#categories').click(function() {
+		if($('#category-dropdown').is(":visible")) {
+			$('#category-dropdown').slideUp(250);
+		}
+		if($('#category-dropdown').is(":hidden")) {
+			$('#category-dropdown').slideDown(250);
+		}
+	});
 
 });
